7c1b4ae633499f1ed80f66697d9cf1be93cc1c92
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2022-01-16 12:02:51 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    if (n < 10) {
+        return n;
+    }
+
+    let nArr = [...n.toString()];
+    if (nArr[nArr.length - 1] === '0') {
+        nArr.pop();
+    }
+    return Number(nArr.reverse().join(''));
 }
